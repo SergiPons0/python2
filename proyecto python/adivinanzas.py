069bead11048f128a279c036a207d4826389f8f3
SergiPons0 <spons20472@iesjoanramis.org>
2023-06-12 09:14:15 +0200

    cambiolocal

--- proyecto python/adivinanzas.py
+++ proyecto python/adivinanzas.py
@@ -1,47 +1,22 @@
-def adivinanzas():
-    print("""
-    -Soy un ave muy colorida,
-    Con plumas brillantes y largas,
-    En las noches, mi canto es oído,
-    ¿Quién soy? Soy el Pavo real.
+import tkinter as tk
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
 
-    -Tengo ramas, pero no hojas,
-    Un tronco, pero no es de árbol,
-    ¿Qué soy? Soy una Silla.
+def cargar_imagen_desde_url(url):
+    response = requests.get(url)
+    imagen = Image.open(BytesIO(response.content))
+    return imagen
 
-    -De noche soy blanco,
-    De día me vuelvo negro,
-    ¿Quién soy? Soy el Búho.
+ventana = tk.Tk()
+ventana.title("Ventana con imagen")
 
-    -Todos me quieren tocar,
-    Pero si me tocan, me destruyen,
-    ¿Quién soy? Soy el Hielo.
+url_imagen = "https://media.gq.com.mx/photos/5ffb662e9274cd36fe35683c/16:9/w_2560%2Cc_limit/messi-cerveza-goles-porteros.jpg"  # Reemplaza con la URL de la imagen que desees mostrar
 
-    *La respuesta tiene que estar contestada con la primera letra mayuscula
-    """)
-a=input("Respuestas 1: ")
-b=input("Respuestas 2: ")
-c=input("Respuestas 3: ")
-d=input("Respuestas 4: ")
-if a=="Pavo real":
-    print("La respuesta 1 es correcta ")
-if not a=="Pavo real":
-    print("La respuesta 1 es incorrecta ")
-if b=="Silla":
-    print("La respuesta 2 es correcta ")
-if not b=="Silla":
-    print("La respuesta 2 es incorrecta ")
-if c=="Buho":
-    print("La respuesta 3 es correcta ")
-if not c=="Buho":
-    print("La respuesta 3 es incorrecta ")
-if d=="Hielo":
-    print("La respuesta 4 es correcta ")
-if not d=="Hielo":
-    print("La respuesta 4 es incorrecta ")
-else:
-    print("Gracias por jugar")
+imagen = cargar_imagen_desde_url(url_imagen)
+imagen_tk = ImageTk.PhotoImage(imagen)
 
+label = tk.Label(ventana, image=imagen_tk)
+label.pack()
 
-
-
+ventana.mainloop()
