cb0c000135334fb13b26c8968105f095aeee2b90
SergiPons0 <spons20472@iesjoanramis.org>
2023-06-15 21:48:49 +0200

    camviolcoal

--- proyecto python/proyecto.py
+++ proyecto python/proyecto.py
@@ -171,21 +171,12 @@ def adivinanzas():
 
 def ventana():
     try: 
-        import imagen.py
+        import imagen
     except:
         ModuleNotFoundError
+        print("Error")
 
-def apy():
-    for i in range(10):
-        a = "https://bus-soades.upc.edu/EquipsTIC/APIv1/tipusInfraestructura"+str((i+1))
-        res=requests.get(a)
-        if res.status_code == 200:
-            dades = res.json()
-            print("El nom del pokémon és: {}".format(dades["chain"]["species"]["name"]))
-            for e in dades["chain"]["evolves_to"]:
-                print("Nom de la seva evolució: ", e["species"]["name"])
-    else:
-        print("No hi ha dades.")
+
         
 
 #PP
